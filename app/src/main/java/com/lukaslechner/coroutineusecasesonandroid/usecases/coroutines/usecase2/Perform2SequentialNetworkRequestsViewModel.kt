1a2b646ca2da7ff1044e9ae2527f878f1a211b8e
dpcks0509 <dpcks0509@gmail.com>
2025-02-26 03:00:45 +0900

    feat: perform2SequentialNetworkRequest

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
@@ -1,14 +1,29 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase2
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class Perform2SequentialNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun perform2SequentialNetworkRequest() {
-        // TODO: Exercise 1
-        // switch to branch "coroutine_course_full" to see solution
+        uiState.value = UiState.Loading
+
+        viewModelScope.launch {
+            try {
+                val recentAndroidVersions = mockApi.getRecentAndroidVersions()
+                val mostRecentVersion = recentAndroidVersions.last()
+                val featuresOfMostRecentVersion =
+                    mockApi.getAndroidVersionFeatures(mostRecentVersion.apiLevel)
+                uiState.value = UiState.Success(featuresOfMostRecentVersion)
+            } catch (exception: Exception) {
+                Timber.e(exception)
+                uiState.value = UiState.Error("Network request failed!")
+            }
+        }
     }
 }
