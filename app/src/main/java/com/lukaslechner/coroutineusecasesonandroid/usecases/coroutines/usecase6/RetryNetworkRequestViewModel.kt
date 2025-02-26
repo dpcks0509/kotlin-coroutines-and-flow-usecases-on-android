ac151abb3a6e7ad380461d678946330dde2051ee
dpcks0509 <dpcks0509@gmail.com>
2025-02-26 18:14:30 +0900

    feat: Implementing retries

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/coroutines/usecase6/RetryNetworkRequestViewModel.kt
@@ -1,14 +1,41 @@
 package com.lukaslechner.coroutineusecasesonandroid.usecases.coroutines.usecase6
 
+import androidx.lifecycle.viewModelScope
 import com.lukaslechner.coroutineusecasesonandroid.base.BaseViewModel
 import com.lukaslechner.coroutineusecasesonandroid.mock.MockApi
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class RetryNetworkRequestViewModel(
     private val api: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun performNetworkRequest() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            val numberOfRetries = 2
+
+            try {
+                repeat(numberOfRetries) {
+                    try {
+                        loadRecentAndroidVersion()
+                        return@launch
+                    } catch (exception: Exception) {
+                        Timber.e(exception)
+                    }
+                }
+                loadRecentAndroidVersion()
+            } catch (exception: Exception) {
+                Timber.e(exception)
+                uiState.value = UiState.Error("Network request failed!")
+            }
+        }
+    }
+
+    private suspend fun loadRecentAndroidVersion() {
+        val recentAndroidVersions = api.getRecentAndroidVersions()
+        uiState.value = UiState.Success(recentAndroidVersions)
     }
 
 }
